213bc4866b3751baeaf38b2a553e26fbbc5393fe
Dawood Usman <110775919+Dawood-Usman@users.noreply.github.com>
2022-10-20 00:07:47 +0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "9pocvy2rg",
+    password: "",
     database: "CRUD_APPLICATION"
 });
 
